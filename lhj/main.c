a057a6bb10a09d8bcc7a5fc0c0ecbd394f1e6777
Hyun Joon1 <jhl259@naver.com>
2026-02-03 17:56:27 +0900

    move OBC_FAULT_Logic to lhj folder

---
+++ lhj/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "fault.h"
+
+int main(void)
+{
+    // 단위 테스트 코드입니다. 하나씩 실행 시키면, 각각의 test_case에 대한 결과값이 출력됩니다..
+    /*Test_Fault_0x01("fault_0x01_test.csv");*/
+    /*Test_Fault_0x02("fault_0x02_test.csv");*/
+    /*Test_Fault_0x03("fault_0x03_test.csv");*/
+    /*Test_Fault_0x04("fault_0x04_test.csv");*/
+    /*Test_Fault_0x05("fault_0x05_test.csv");*/
+    /*Test_Fault_0x06("fault_0x06_test.csv");*/
+    /*Test_Fault_0x07("fault_0x07_test.csv");*/
+    /*Test_Fault_0x08("fault_0x08_test.csv");*/
+    /*Test_Fault_0x09("fault_0x09_test.csv");*/
+    /*Test_Fault_0x0A("fault_0x0A_test.csv");*/
+    /*Test_Fault_0x0B("fault_0x0B_test.csv");*/
+    /*Test_Fault_0x0C("fault_0x0C_test.csv");*/
+    return 0;
+}
